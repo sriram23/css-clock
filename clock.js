9ae138063ce2067a2a7cf6a27b768574561e4212
Sriram <sriramsubramanian23@gmail.com>
2023-01-14 15:34:37 +0530

    Added timezone offset

--- clock.js
+++ clock.js
@@ -6,6 +6,15 @@ const minute = document.querySelector(".min");
 const sec = document.querySelector(".sec");
 // Digital time
 const time = document.querySelector("#time");
+// Digital offset
+const offset = document.querySelector("#offset");
+
+function getOffset(mins) {
+  const tempMin = mins > 0 ? mins : -1 * mins;
+  const hrs = Math.floor(tempMin / 60);
+  const min = tempMin % 60;
+  return "("+(mins > 0 ? "-" : "+") + hrs.toString().padStart(2,"0")+":"+min.toString().padStart(2,"0")+" UTC)";
+}
 
 function getAngle() {
   time.innerHTML =
@@ -14,7 +23,7 @@ function getAngle() {
     new Date().getMinutes().toString().padStart(2, "0") +
     ":" +
     new Date().getSeconds().toString().padStart(2, "0");
-
+  offset.innerHTML = getOffset(new Date().getTimezoneOffset());
   // 1 hr === 30 deg
   const hrAngle = (new Date().getHours() % 12) * 30 + startValue;
   // 1 min || 1 sec === 6 deg
